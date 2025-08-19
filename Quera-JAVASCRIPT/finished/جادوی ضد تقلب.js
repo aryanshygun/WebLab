7a14877378946779a5f1ce8c456e30c3f46742b3
aryanshygun <ryan.shygun@gmail.com>
2025-08-19 19:00:03 +0330

    restructuring

--- Quera-JAVASCRIPT/finished/جادوی ضد تقلب.js
+++
@@ -1,7 +0,0 @@
-// https://quera.org/college/16059/chapter/64206/lesson/218101/?comments_page=1&comments_filter=ALL&submissions_page=1
-
-x = readline()
-
-console.log(`aji maji la taraji ${x}.`)
-console.log('man ghol midam dige taghallob nakonam.')
-console.log('vagarna khoda mano soosk kone!')
--- Quera-JAVASCRIPT/finished/سیونِبِ سکعَرب.js
+++
@@ -1,8 +0,0 @@
-// https://quera.org/college/16059/chapter/64205/lesson/218092/?comments_page=1&comments_filter=ALL
-
-x = readline()
-xx = x.toString()
-
-xrev = xx.split('').reverse().join('')
-
-console.log(xrev)
